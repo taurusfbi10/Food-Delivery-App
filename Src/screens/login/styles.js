94624c4e7cd6d9a6c6ee845a2483111587832a3f
NVinh7350 <vanvinhqn7350@gmail.com>
2022-10-10 21:57:20 +0700

    Create component fieldButton, fieldTextInput, completed UI screen Login

--- Src/screens/login/styles.js
+++ Src/screens/login/styles.js
@@ -3,8 +3,37 @@ import { heightScreen, widthScreen, colors } from '../../utility'
 
 export default StyleSheet.create({
     container : {
-        height : heightScreen * 0.5,
+        height : heightScreen,
         width: widthScreen,
-        backgroundColor: colors.GREEN_BLAND
-    }
+    },
+    containerHeader: {
+        height : heightScreen * 0.4,
+        width: widthScreen,
+        paddingVertical: heightScreen * 0.03,
+        paddingHorizontal: widthScreen * 0.075,
+        // borderWidth:1
+    },
+    imageTitle: {
+        height: heightScreen * 0.2,
+        marginVertical: heightScreen* 0.02,
+        aspectRatio: 1,
+        borderRadius: 100,
+        alignSelf:'center'
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: '800',
+        color: colors.ORANGE_DARK
+    },
+    containerBody: {
+        height : heightScreen * 0.55,
+        paddingHorizontal: widthScreen * 0.075,
+        
+    },
+    textForgotPW: {
+        fontSize: 16,
+        color:colors.BLUE_DARK,
+        alignSelf:'flex-end',
+        marginVertical:heightScreen * 0.02
+    },
 })
--- Src/utility/colors/index.js
+++ Src/utility/colors/index.js
@@ -3,7 +3,7 @@ export const WARNING = 'red';
 export const GREEN_DARK = '#25CE09'
 export const GREEN_BLAND = '#A1F999'
 
-export const GRAY_DARK = '#B0B0B0'
+export const GRAY_DARK = '#939090'
 export const GRAY_BLAND = '#B7BABD'
 export const GRAY_CLOUD = '#F0F0F0'
 
@@ -11,4 +11,4 @@ export const BLUE_DARK = '#1976F2'
 export const BLUE_BLAND = '#C7DFFF'
 export const WHITE = '#FFFFFF'
 export const BLACK = '#000000'
-export const ORANGE= '#09F134'
+export const ORANGE_DARK= '#FD5308'
